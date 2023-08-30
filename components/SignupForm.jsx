f41425f7c66e99f3b96158df96029b83f066b8a6
Ryan P <rdpinkstaff@gmail.com>
2023-08-29 19:43:42 -0600

    Minor styling details

--- components/SignupForm.jsx
+++ components/SignupForm.jsx
@@ -16,21 +16,21 @@ function SignupForm({ signup, setSignup }) {
            max-[375px]:flex-col-reverse max-[375px]:w-full max-[375px]:border-none max-[375px]:rounded-none">
         <div className="w-50 h-50 flex flex-col justify-center m-10 max-[375px]:ml-4 max-[375px]:mr-4">
   
-          <h1 className="font-bold text-5xl mb-4 mr-10 ml-10 max-[375px]:text-3xl max-[375px]:ml-0">Stay updated!</h1>
+          <h1 className="font-bold text-5xl mb-4 mr-10 ml-10 max-[375px]:text-4xl max-[375px]:ml-0 max-[375px]:mb-6">Stay updated!</h1>
         
-          <p className="mb-4">Join 60,000+ product managers receiving monthly updates on:</p>
-          <ul className="list-image-none list-inside mb-4">
+          <p className="mb-4 max-[375px]:mb-5">Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul className="list-image-none list-inside mb-4 max-[375px]:mb-6">
             {/* <li className="mb-2"><span className="ml-2">Product discovery and building what matters</span></li> */}
-            <li className="flex mb-2"><img className="mr-2" src="../assets/images/icon-list.svg" /><span>Product discovery and building what matters</span></li>
+            <li className="flex mb-2 items-start"><img className="mr-3" src="../assets/images/icon-list.svg" /><span>Product discovery and building what matters</span></li>
 
             {/* <li className="mb-2"><span className="ml-2">Measuring to ensure updates are a success</span></li> */}
-            <li className="flex mb-2"><img className="mr-2" src="../assets/images/icon-list.svg" /><span>Measuring to ensure updates are a success</span></li>
+            <li className="flex mb-2 items-start"><img className="mr-3" src="../assets/images/icon-list.svg" /><span>Measuring to ensure updates are a success</span></li>
             {/* <li className="mb-2"><span className="ml-2">And much more!</span></li>   */}
-            <li className="flex mb-2"><img className="mr-2" src="../assets/images/icon-list.svg" /><span>And much more!</span></li>
+            <li className="flex mb-2 items-start"><img className="mr-3" src="../assets/images/icon-list.svg" /><span>And much more!</span></li>
 
           </ul>
           
-          <form action="#" className="flex flex-col" onSubmit={validateEmail}> 
+          <form action="#" className="flex flex-col max-[375px]:mb-6" onSubmit={validateEmail}> 
             <label className="block text-sm font-bold mb-2" htmlFor="email">Email address</label>
             <input className="appearance-none border rounded w-full px-2 py-3 mb-6 cursor-pointer" 
                     type="email" name="email" id="email" placeholder="email@company.com" 

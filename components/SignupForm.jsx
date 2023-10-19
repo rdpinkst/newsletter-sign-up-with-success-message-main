70f89c0d5f6bb56f696002915f781c58de88f455
Ryan P <rdpinkstaff@gmail.com>
2023-10-18 18:00:37 -0600

    Made more responsive

--- components/SignupForm.jsx
+++ components/SignupForm.jsx
@@ -21,13 +21,13 @@ function SignupForm({ signup, setSignup, email, setEmail }) {
 
     return (
       <div className="flex border border-black rounded-3xl bg-white w-full 
-           max-[375px]:flex-col-reverse max-[375px]:w-full max-[375px]:border-none max-[375px]:rounded-none">
-        <div className="w-50 h-50 flex flex-col justify-center m-10 max-[375px]:ml-4 max-[375px]:mr-4">
+           max-[850px]:flex-col-reverse max-[850px]:w-full max-[850px]:border-none max-[850px]:rounded-none">
+        <div className="w-50 h-50 flex flex-col justify-center m-10 max-[850px]:ml-4 max-[850px]:mr-4">
   
-          <h1 className="font-bold text-6xl mb-4 mr-10 ml-10 max-[375px]:text-4xl max-[375px]:ml-0 max-[375px]:mb-6">Stay updated!</h1>
+          <h1 className="font-bold text-6xl mb-4 mr-10 ml-10 max-[850px]:text-4xl max-[850px]:ml-0 max-[850px]:mb-6">Stay updated!</h1>
         
-          <p className="mb-4 text-lg max-[375px]:mb-5">Join 60,000+ product managers receiving monthly updates on:</p>
-          <ul className="list-image-none text-lg list-inside mb-4 max-[375px]:mb-6">
+          <p className="mb-4 text-lg max-[850px]:mb-5">Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul className="list-image-none text-lg list-inside mb-4 max-[850px]:mb-6">
             <li className="flex mb-2 items-start"><img className="mr-4" src="../assets/images/icon-list.svg" /><span>Product discovery and building what matters</span></li>
             <li className="flex mb-2 items-start"><img className="mr-4" src="../assets/images/icon-list.svg" /><span>Measuring to ensure updates are a success</span></li>
             <li className="flex mb-2 items-start"><img className="mr-4" src="../assets/images/icon-list.svg" /><span>And much more!</span></li>
@@ -35,8 +35,8 @@ function SignupForm({ signup, setSignup, email, setEmail }) {
           </ul>
           
           <form action="#" className="flex flex-col" onSubmit={validateEmail}> 
-            <label className="block text-sm font-bold mb-2 max-[375px]:text-xs" htmlFor="email">Email address</label>
-            <input className="appearance-none border rounded w-full px-2 py-3 mb-1 cursor-pointer max-[375px]:rounded-lg" 
+            <label className="block text-sm font-bold mb-2 max-[850px]:text-xs" htmlFor="email">Email address</label>
+            <input className="appearance-none border rounded w-full px-2 py-3 mb-1 cursor-pointer max-[850px]:rounded-lg" 
                     type="email" name="email" id="email" placeholder="email@company.com" 
                     value={email} onChange={inputEmail} />
             <small className="h-1 mb-6 text-red-800">{error}</small>
@@ -49,9 +49,9 @@ function SignupForm({ signup, setSignup, email, setEmail }) {
           <picture>
             <source className="w-full" 
               srcSet="../assets/images/illustration-sign-up-mobile.svg" 
-              media="(max-width: 375px)"
-              sizes="375px" />
-            <img className="m-4 max-[375px]:m-0" src="../assets/images/illustration-sign-up-desktop.svg" alt="" />
+              media="(max-width: 850px)"
+              sizes="850px" />
+            <img className="m-4 max-w-none max-[850px]:m-0 max-[850px]:w-full" src="../assets/images/illustration-sign-up-desktop.svg" alt="" />
           </picture>
           
         </div>
